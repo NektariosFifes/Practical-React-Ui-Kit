f29ba48086b792ffb1c1913bf0374a6d9c3231d1
NektariosFifes <61620751+NektariosFifes@users.noreply.github.com>
2021-02-17 13:34:41 +0200

    ...

--- src/Library/TextPractical/TextComp.tsx
+++ src/Library/TextPractical/TextComp.tsx
@@ -6,12 +6,26 @@ interface Component_Props {
     preset_Heading_Paragraph_Title?:boolean
     preset_Heading_BelowTitle_Description?:boolean
     size?: number | string
+    a?: boolean
+    br?: boolean
+    code?: boolean
+    cite?: boolean
+    del?: boolean
+    em?: boolean
+    small?: boolean
 }
 
 const Default_Properties =    {
     preset_Heading_BelowTitle_Description:false,
     preset_Heading_Paragraph_Title:false,
     preset_Heading_Title:false,
+    a: false,
+    br: false,
+    code: false,
+    cite: false,
+    del: false,
+    em:false,
+    small:false,
 }
 
 
@@ -23,13 +37,50 @@ export type TextCompProps = typeof Default_Properties & Component_Props & BasicH
 
 type HtmlElementsList = { [key in keyof JSX.IntrinsicElements]?: boolean }
 
+type PropsArray =  Array<keyof Component_Props>
 const TextComp : React.FC<React.PropsWithChildren<TextCompProps>> = ({
         preset_Heading_BelowTitle_Description,
         preset_Heading_Title,
         preset_Heading_Paragraph_Title,
         size,
+        a,
+        br,
+        code,
+        cite,
+        del,
+        em,
+        small,
     ...props
-    })=>
-{
+    })=> {
+    const AvailableAppendableElements: HtmlElementsList = {
+        preset_Heading_BelowTitle_Description,
+        preset_Heading_Title,
+        preset_Heading_Paragraph_Title,
+        size,
+
+    }
+    const AvailableAppendableInlineElements: HtmlElementsList = {
+        a, br, code, cite, del, em, small
+    }
+
+    const AppendbleElements_Sanitized = Object.keys(AvailableAppendableElements)
+        .filter((element: keyof Component_Props) => {
+                AvailableAppendableElements[element]
+            }
+    ) as PropsArray
+
+    const AppendbleInlineElements_Sanitized = Object.keys(AvailableAppendableInlineElements)
+        .filter((element: keyof Component_Props) => {
+                AvailableAppendableInlineElements[element]
+            }
+    ) as PropsArray
+
+
+
+
+
+
 
 }
+
+
